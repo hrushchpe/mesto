690dc53106e5ddf62a0ee539c95ecd6a99df7be4
Yuliya Khrushcheva <khrushcheva-yuliya@ya.ru>
2022-10-17 16:54:48 +0300

    исправлены ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,36 @@
 let popUp = document.querySelector('.popup');
 let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector(".popup__input_name");
-let jobInput = document.querySelector(".popup__input_description");
+let nameInput = document.querySelector(".popup__input_type_name");
+let jobInput = document.querySelector(".popup__input_type_description");
 let openPopupButton = document.querySelector('.profile__edit-button');
 let closePopupButton = document.querySelector('.popup__close-button');
 let profileName = document.querySelector(".profile__name");
 let profileDescription = document.querySelector(".profile__description");
 let saveButton = document.querySelector('.popup__save-button');
 
-function popUpToggle() {
-    popUp.classList.toggle('popup_opened');
+function popUpOpen() {
+    popUp.classList.add('popup_opened');
 }
-
-openPopupButton.addEventListener('click', () => {
-    popUpToggle();
+function popUpClose() {
+    popUp.classList.remove('popup_opened');
+}
+function inputToggle () {
     nameInput.value = profileName.textContent;
     jobInput.value = profileDescription.textContent;
+}
+openPopupButton.addEventListener('click', () => {
+    popUpOpen();
+    inputToggle ();
 })
 closePopupButton.addEventListener('click', () => {
-    popUpToggle();
+    popUpClose();
 })
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    profileName.textContent = nameInput.value;
+    profileName.textContent = nameInput.value; 
     profileDescription.textContent = jobInput.value;
-    popUpToggle();
+    popUpClose();
 
 }
 formElement.addEventListener('submit', formSubmitHandler);
